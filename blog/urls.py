2285098e82594b16827261d005b774e062f53bc9
Igor Kunkevich <igor.kunkevich@gmail.com>
2020-05-19 01:02:16 +0300

    git zaliv

---
+++ blog/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+
+from blog.views.comment import CommentCreate
+from blog.views.home import home
+from blog.views.post import PostView, PostCreate, PostUpdate, PostDelete
+
+app_name = 'blog'
+urlpatterns = [
+    path('', home, name='home'),
+    path('<str:username>', home, name='user_posts'),
+    path('post/<int:pk>/', PostView.as_view(), name='post'),
+    path('post/create/', PostCreate.as_view(), name='create_post'),
+    path('post/create/<int:pk>/update', PostUpdate.as_view(), name='update_post'),
+    path('post/<int:pk>/delete/', PostDelete.as_view(), name='delete_post'),
+    path('post/<int:pk>/comment/', CommentCreate.as_view(), name='create_comment')
+]
