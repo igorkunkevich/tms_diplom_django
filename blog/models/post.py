f2e117a21f94b66e08af801a1469952dfba1bd3e
Igor Kunkevich <igor.kunkevich@gmail.com>
2020-05-25 00:59:23 +0300

    rework

--- blog/models/post.py
+++ blog/models/post.py
@@ -7,7 +7,7 @@ class Post(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     title = models.CharField(max_length=80)
     body = models.TextField()
-    img = models.ImageField(height_field=200, width_field=200)
+    image = models.ImageField(upload_to="media/image")
     pub_date = models.DateTimeField('date published', auto_now_add=True)
 
     def get_absolute_url(self):
